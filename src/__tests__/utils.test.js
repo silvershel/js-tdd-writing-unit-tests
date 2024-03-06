b2d7cf6a0e82c815c898998cae4c2f0dac2a87d3
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-03-06 13:33:44 -0500

    completed codealong

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -6,4 +6,12 @@ describe("pointsForWord", () => {
         const points = pointsForWord(word);
         expect(points).toBe(7);
     });
+
+    it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+      
+        const points = pointsForWord(word);
+      
+        expect(points).toBe(7);
+      });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
 export function pointsForWord(word) {
     let points = 0;
-    for(const char of word ) {
-        if(["a", "e", "i", "o", "u"].includes(char)) {
-            points += 1;
-        } else {
-            points += 2;
-        }
+    for (const char of word) {
+      points += /[aeiou]/i.test(char) ? 1 : 2;
     }
     return points;
-}
+  }
